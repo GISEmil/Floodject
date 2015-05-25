d575b5154d44288c6d359822722d4c89f94e0757
Emil Møller Rasmussen <emil.moeller.rasmussen@gmail.com>
2015-05-25 14:47:02 +0200

    Adding the latest working versions

--- PyWPS/wps/processes/testing.py
+++ PyWPS/wps/processes/testing.py
@@ -3,6 +3,13 @@ import os
 import random
 import string
 
+import json
+
+from osgeo import ogr
+import os
+
+import logging
+
 
 # Preliminary Settings #
 ########################
@@ -11,14 +18,14 @@ import string
 
 random_file1 = ''.join([random.choice(string.ascii_letters + string.digits) for n in xrange(8)])
 random_folder1 = ''.join([random.choice(string.ascii_letters + string.digits) for n in xrange(6)])
-random_file2 = ''.join([random.choice(string.ascii_letters + string.digits) for n in xrange(8)])
-random_folder2 = ''.join([random.choice(string.ascii_letters + string.digits) for n in xrange(6)])
 
 # Amount of input is fixed, output is dependent on the number of loops.
 
 #Define empty lists for later use
-
+outputnames_vect=[]
+outputnames_rast=[]
 areaList=[]
+
 
 #Some variables can't be empty when initiating the script
 
@@ -28,7 +35,7 @@ pourpoint_vect = ""
 maxlevel_input = 3
 
 #Flood increment
-interval_input = 3
+interval_input = 0.01
 
 #Total amount of loops
 loop_no = int((maxlevel_input/interval_input))
@@ -45,7 +52,7 @@ class Flooding(WPSProcess):
             abstract = "This process is used to flood a DEM with water",
             version = "1.0",
             #grassLocation = None,
-            grassLocation = True,
+            grassLocation = "/home/ubuntu/grassdata/WGS_1984",
             statusSupported = True,
             storeSupported = True)
 
@@ -54,7 +61,7 @@ class Flooding(WPSProcess):
 
         self.rasterin=self.addComplexInput(identifier='rasterin',maxmegabites=10000,title="input image",minOccurs=1,maxOccurs=1,formats = [{'mimeType': 'image/tiff'}, {'mimeType': 'image/geotiff'}, {'mimeType': 'application/geotiff'}, {'mimeType': 'application/x-geotiff'}])
 
-        self.vectorin = self.addComplexInput(identifier="vectorin",title="Input point")
+        self.vectorin = self.addComplexInput(identifier="vectorin",title="Input point",formats = [{"mimeType":"text/json","encoding":"utf-8","schema":None}])
 
         # Define output #
         #################
@@ -63,40 +70,21 @@ class Flooding(WPSProcess):
 
         self.outputImage1=self.addComplexOutput(identifier="output1",title="output image", asReference=True)
 
-        self.outputVector1=self.addComplexOutput(identifier="outputVector1",title="output vector", asReference=True)
-
+        self.outputVector=self.addComplexOutput(identifier="output2",title="output vector", asReference=True)
 
     def execute(self):
 
-    	# Importing the provided raster and and vector data
         original = 'original'
+
+        firstvector = ""
+        firstrun = 0
+
+        pourpoint_vect = 'pourpoint_vect_'
+
+    	# Importing the provided raster and and vector data
         self.cmd(['r.in.gdal','input=%s' % self.rasterin.getValue(),'output=%s' % original,'-o'])
-
-        if os.path.isfile('/tmp/%s/%s' % (random_folder1, random_file1): #This should be a relative path by the way, as several requests will try and read the same file
-
-	    self.cmd(['r.report', 'map=%s' % selected_ocean_rast, 'units=k', 'output=%s' % (random_file])
-
-        actual_increment = str(actual_loop*interval_input)
-
-  	    tempTxt = open('temp.txt', 'r')
-
-        for row in tempTxt:
-        	if row[1]== actual_increment[0]:
-        		AreaOut=row.split('|')
-        		areaList.append(AreaOut[3]) #This should be explained properly
-        tempTxt.close()
-
         ocean_point = 'ocean_point'
-        self.cmd(['v.in.ogr','dsn=%s' % self.vectorin.getValue(), 'type=point','output=%s' % ocean_point,'-o','--verbose'])
-
-        #Convert entire raster to values of 0
-        self.cmd(['r.mapcalc','%s = if( %s <=0, 0, 0)' % ('b_area',original)])
-
-	    #Converting the original tif into a bounding box
-        self.cmd(['r.to.vect','input=b_area','output=b_polygon','feature = area'])
-
-	    #Extract the land from DEM
-        self.cmd(['r.mapcalc', '%s = if(%s != 0, %s, null())' % ('Xland',original,'original')])
+        self.cmd(['v.in.ogr','dsn=%s' % self.vectorin.getValue(), 'type=point','output=%s' % ocean_point,'-o'])
 
         ##############################
         ##                          ##
@@ -132,69 +120,90 @@ class Flooding(WPSProcess):
             #Do something ??
             self.cmd(['r.mapcalc', '%s = if( %s == 0, (%s * %s), null())' % (selected_ocean_rast, intermediate, interval_input, actual_loop)])
 
-            if os.path.isfile('/tmp/temp.txt'): #This should be a relative path by the way, as several requests will try and read the same file
-            	os.remove('/tmp/temp.txt')
+	        #Export the vectors and rasters
+            outputnames_rast.append(selected_ocean_rast)
 
-    	    self.cmd(['r.report', 'map=%s' % selected_ocean_rast, 'units=k', 'output=temp.txt'])
+            outputnames_vect.append(selected_ocean)
 
-            actual_increment = str(actual_loop*interval_input)
+            self.cmd(['v.to.db', 'map=%s' % selected_ocean, 'option=area','columns=value'])
 
-      	    tempTxt = open('temp.txt', 'r')
+            area = self.cmd(["v.db.select", '-c', 'map=%s' % selected_ocean, 'col=value'])
 
-            for row in tempTxt:
-            	if row[1]== actual_increment[0]:
-            		AreaOut=row.split('|')
-            		areaList.append(AreaOut[3]) #This should be explained properly
-            tempTxt.close()
+            logging.debug("%s" % area)
+
+            logging.debug("%f" % float(area))
+
+            areaList.append(float(area)/1000000)
+
+            logging.debug("%s" % areaList)
 
             old_loop = actual_loop -1
 
-            ###########################################
-            #                                         #
-            # Check to see if we have a critical area #
-            #                                         #
-            ###########################################
 
             if float(areaList[actual_loop]) - float(areaList[old_loop]) > 0.02:
 
-        		self.cmd(['v.overlay', 'ainput = %s' % selected_ocean,
-                    	'binut = old_flood', 'operator=not', 'output=subtructed_flood'])
+                firstrun = firstrun + 1
 
-            	self.cmd(['v.to.db', 'map=subtructed_flood', 'option=area', 'columns=a_value'])
+                pourpoint='pourpoint_'+str(actual_loop)
 
-            	self.cmd(['v.extract', 'input=subtracted_flood', 'output=subtracted_flood_extract', 'where=a_value > 1000'])
+                pourpoint_vect = 'pourpoint_vect_' + str(actual_loop)
 
-            	self.cmd(['v.to.rast', 'input=subtracted_flood_extract','output=subtracted_raster','use=cat'])
+                self.cmd(['v.overlay', 'ainput=%s' % selected_ocean, 'binput=old_flood', 'operator=not', 'output=subtructed_flood'])
 
-        		self.cmd(['r.mapcalc', '%s=if( isnull(%s), %s, %s)' % ('merged', 'subracted_raster', 'old_flood_rast', selected_ocean_rast)])
+                self.cmd(['v.to.db', 'map=subtructed_flood', 'option=area', 'columns=a_value'])
 
-        		self.cmd(['r.neighbors', 'input=merged', 'output=diversity', 'method=diversity'])
+                self.cmd(['v.extract', 'input=subtructed_flood', 'output=subtracted_flood_extract', 'where=a_value > 1000'])
 
-        		pourpoint='pourpoint_'+str(actual_loop)
-        		pourpoint_vect = 'pourpoint_vect_' + str(actual_loop)
+                self.cmd(['v.to.rast', 'input=subtracted_flood_extract','output=subtracted_raster','use=cat'])
 
-        		self.cmd(['r.mapcalc', '%s=if(%s == 2, %s, null())' % (pourpoint, 'diversity', 'diversity')])
+                self.cmd(['r.mapcalc', '%s=if( isnull(%s), %s, %s)' % ('merged', 'subtracted_raster', 'old_flood_rast', selected_ocean_rast)])
 
-        		self.cmd(['r.to.vect', 'input=%s' % pourpoint, 'output=%s' % pourpoint_vect, 'feature=point'])
+                self.cmd(['r.neighbors', 'input=merged', 'output=diversity', 'method=diversity'])
 
-        		self.cmd(['g.remove', '-f', 'rast=sub*'])
-        		self.cmd(['g.remove', '-f', 'vect=sub*'])
-        		self.cmd(['g.remove', '-f', 'rast=merged*'])
-        		self.cmd(['g.remove', '-f', 'rast=diversity*'])
+                self.cmd(['r.mapcalc', '%s=if(%s == 2, %s, null())' % (pourpoint, 'diversity', 'diversity')])
 
-        	    self.cmd(['g.remove', '-f', 'vect=old_flood'])
+                self.cmd(['r.to.vect', 'input=%s' % pourpoint, 'output=%s' % pourpoint_vect, 'feature=point'])
 
-        	    self.cmd(['g.remove', '-f', 'rast=old_flood_rast'])
+                if firstrun == 1:
+                    firstvector = pourpoint_vect
+                else:
+                    self.cmd(['v.patch','--overwrite','-a','input=%s' % pourpoint_vect, 'output=%s' % firstvector])
 
-        	    self.cmd(['g.copy', 'vect = %s, %s' % (selected_ocean, 'old_flood')])
+                #self.cmd(['g.remove', '-f', 'rast=sub*'])
 
-        	    self.cmd(['g.copy', 'rast = %s, %s' % (selected_ocean_rast, 'old_flood_rast')])
+                #self.cmd(['g.remove', '-f', 'vect=sub*'])
+
+                #self.cmd(['g.remove', '-f', 'rast=merged*'])
+
+                #self.cmd(['g.remove', '-f', 'rast=diversity*'])
+
+            self.cmd(['g.remove', '-f', 'vect=old_flood'])
+
+            self.cmd(['g.remove', '-f', 'rast=old_flood_rast'])
+
+            self.cmd(['g.copy', 'vect=%s,%s' % (selected_ocean, 'old_flood')])
+
+            self.cmd(['g.copy', 'rast=%s,%s' % (selected_ocean_rast, 'old_flood_rast')])
 
             actual_loop = actual_loop + 1
 
-        # Create waterdepths
+
         self.cmd(['r.mapcalc', '%s = %s - %s' % ('WaterDepth', selected_ocean_rast, original)])
 
+        outputImage = "outputimage.jpg"
+        self.cmd(['r.out.gdal','format=JPEG','input=%s' % selected_ocean_rast, 'output=%s' % outputImage])
+
+        outputImage1 = "outputimage1.tif"
+        self.cmd(['r.out.gdal','format=GTiff','input=%s' % selected_ocean_rast, 'output=%s' % outputImage1])
+
+        outputVector1 = "outputvector.geojson"
+        self.cmd(['v.out.ogr','format=GeoJSON','type=point','input=%s' % firstvector,'dsn=%s' % outputVector1])
+
+        self.outputImage0.setValue(outputImage)
+
+        self.outputImage1.setValue(outputImage1)
+
+        self.outputVector.setValue(outputVector1)
 
 
         ######################################
@@ -202,12 +211,3 @@ class Flooding(WPSProcess):
         ## All the outputs are defined here ##
         ##                                  ##
         ######################################
-
-        outputImage = "outputimage.jpg"
-        self.cmd(['r.out.gdal','format=JPEG','input=WaterDepth', 'output=%s' % outputImage])
-
-        outputImage1 = "outputimage1.tif"
-        self.cmd(['r.out.gdal','format=GTiff','input=WaterDepth', 'output=%s' % outputImage1])
-
-        outputVector1 = "pourpoints.geojson"
-        self.cmd(['v.out.ogr','format=GeoJSON','type=line','input=%s' % pourpoint_vect,'dsn=%s' % outputVector1])
